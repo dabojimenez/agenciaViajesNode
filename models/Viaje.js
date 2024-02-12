49228408c1f8f075dd0bff181954f5dd22b56892
dabojimenez <dabojimenez16@hotmail.com>
2024-02-12 17:21:14 -0500

    Create Viaje.js

---
+++ models/Viaje.js
@@ -0,0 +1,30 @@
+import Sequelize from "sequelize";
+import db from "../config/db";
+
+// en el objeto tenemos que definir cada una de las tablas
+export const Viaje = db.define('viajes', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    precio: {
+        type: Sequelize.STRING
+    },
+    fecha_id: {
+        type: Sequelize.DATE
+    },
+    fecha_vuelta: {
+        type: Sequelize.DATE
+    },
+    imagen: {
+        type: Sequelize.STRING
+    },
+    descripcion: {
+        type: Sequelize.STRING
+    },
+    disponibles: {
+        type: Sequelize.STRING
+    },
+    slug: {
+        type: Sequelize.STRING
+    },
+})
